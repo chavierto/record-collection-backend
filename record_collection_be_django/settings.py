038a92533fb783e0398f0428684cf7dc72386d97
chavierto <hola@xavierlujan.com>
2020-09-08 11:54:09 -0400

    Install Django REST Framework

--- record_collection_be_django/settings.py
+++ record_collection_be_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'record_collection_be',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -84,6 +85,14 @@ DATABASES = {
     }
 }
 
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
